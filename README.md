4ff406a3f7a9f1e8fb36baca14f4001fdfe4f149
Nhat-Dinh Nguyen Nguyen <98847679+nhatdinhnguyen@users.noreply.github.com>
2022-02-11 02:55:47 +0000

    codes yay

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 ## Team Members
 
-* Team Member 1:
-* Team Member 2:
+* Team Member 1: Nhat-Dinh Nguyen
+* Team Member 2: Luke Zink
 * Team Member 3:
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# empty list
+my_list=[]
+i = 0
+x = int(input("input a number "))
+z = int(input("input another number "))
+y = int(input("input another number "))
+w = int(input("input another number "))
+my_list.append(x)
+my_list.append(z)
+my_list.append(y)
+my_list.append(w)
+for i in my_list:
+  print(i)
 
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
+
+print(my_list)
